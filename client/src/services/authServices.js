e9c313256434ca8c796d89486c59772a73d3cbc1
aminaouini <aminaouini565@gmail.com>
2020-05-03 03:29:20 +0100

    fixed register service path

--- client/src/services/authServices.js
+++ client/src/services/authServices.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default {
     register: async (creds) => {
-        axios.post('http://localhost:4406/register', creds)
+        axios.post('http://localhost:4404/register', creds)
             .then(res => {
                 console.log(res)
             })
