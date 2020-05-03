62af73f0779b48f05a0703caac212ac9559fc1df
aminaouini <aminaouini565@gmail.com>
2020-05-03 22:27:07 +0100

    done with register action

--- client/src/components/auth/register.js
+++ client/src/components/auth/register.js
@@ -19,19 +19,35 @@ class Register extends Component {
     }
     onSubmit(e) {
         e.preventDefault();
-        console.log(this.state)
+        let validEmail = /@./;
 
-        // console.log(e.target.firstname.value)
+        if (this.state.firstname.length === 0 ||
+            this.state.lastname.length === 0 ||
+            this.state.email.length === 0 ||
+            this.state.password.length === 0 ||
+            this.state.birthDate.length === 0 ||
+            !validEmail.test(this.state.email)
+        ) {
+            if (this.state.firstname.length === 0) alert('first name is empty')
+            if (this.state.lastname.length === 0) alert('last name is empty')
+            if (this.state.email.length === 0) alert('email is empty')
+            else if (!validEmail.test(this.state.email)) alert('email is not valid')
+            if (this.state.password.length === 0) alert('password is short')
+            if (this.state.birthDate.length === 0) alert('birth date is empty')
+        } else {
+
+            this.props.registerAction(this.state)
+        }
     }
     render() {
         return (
             <div>
                 <form onSubmit={this.onSubmit.bind(this)}>
-                    <input type="text" name="firsname" placeholder="First Name" maxLength={30} onChange={this.onchange.bind(this)}></input><br></br>
-                    <input type="text" name="firstname" placeholder="Last Name" maxLength={30} onChange={this.onchange.bind(this)}></input><br></br>
-                    <input type="email" name="email" placeholder="Email" maxLength={250} onChange={this.onchange.bind(this)}></input><br></br>
-                    <input type="password" name="password" placeholder="Password" maxLength={250} onChange={this.onchange.bind(this)}></input><br></br>
-                    <input type="date" name="birthDate" onChange={this.onchange.bind(this)}></input><br></br>
+                    <input type="text" name="firstname" placeholder="First Name" maxLength={30} onChange={this.onchange.bind(this)} required ></input><br></br>
+                    <input type="text" name="lastname" placeholder="Last Name" maxLength={30} onChange={this.onchange.bind(this)} required ></input><br></br>
+                    <input type="email" name="email" placeholder="Email" maxLength={250} onChange={this.onchange.bind(this)} required ></input><br></br>
+                    <input type="password" name="password" placeholder="Password" maxLength={250} onChange={this.onchange.bind(this)} required ></input><br></br>
+                    <input type="date" name="birthDate" onChange={this.onchange.bind(this)} required ></input><br></br>
                     <button type="submit" onClick={this.onSubmit.bind(this)}>Register</button>
 
                 </form>
@@ -46,5 +62,5 @@ const mapStateToProps = state => ({
     auth: state.auth
 });
 
-export default connect(mapStateToProps, registerAction)(Register);
+export default connect(mapStateToProps, { registerAction })(Register);
 // export default Register;
