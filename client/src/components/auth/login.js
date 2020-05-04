d246de5da78b08ffd52b98ff4b208eea62000c89
aminaouini <aminaouini565@gmail.com>
2020-05-04 05:31:00 +0100

    added login front

---
+++ client/src/components/auth/login.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { loginAction } from '../../actions/authActions';
+import { Link, Redirect } from 'react-router-dom';
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: ""
+        }
+    }
+    onchange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+    onSubmit(e) {
+        e.preventDefault();
+        this.props.loginAction(this.state);
+    }
+    render() {
+        return this.props.auth.openedAcc ? (
+            <div>
+                <form>
+                    <input maxLength={250} onChange={this.onchange.bind(this)} type="email" placeholder="Email" name="email"></input><br></br>
+                    <input maxLength={250} onChange={this.onchange.bind(this)} type="password" placeholder="Password" name="email"></input><br></br>
+                    <button>Log In</button>
+                </form>
+                <Link to="/register">Register</Link>
+            </div>
+        ) : <Redirect to="/"></Redirect>
+    }
+}
+
+let mapPropstoState = state => ({
+    auth: state.auth
+})
+
+export default connect(mapPropstoState, { loginAction })(Login);
