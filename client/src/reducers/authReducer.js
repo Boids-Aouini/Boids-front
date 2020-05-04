c371aa11a0f82bdd2c139dc4e2d475233e3b488c
aminaouini <aminaouini565@gmail.com>
2020-05-04 08:40:55 +0100

    fixed doc to auth reducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -8,11 +8,11 @@ export default function (state = initialState, action) {
     switch (action.type) {
         case REGISTER:
             return {
-                openedAcc: action.openedAcc // set openedAcc to true once dispatch excuted in register action
+                openedAcc: action.openedAcc // set openedAcc to true that is set in dispatch in register action
             }
         case OPEN_ACC:
             return {
-                openedAcc: action.openedAcc // set openedAcc to true once dispatch excuted in login action
+                openedAcc: action.openedAcc // set openedAcc to true that is set in dispatch in login action
             }
         default:
             return state;
