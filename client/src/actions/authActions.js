c05ecd7f95fd8ba20c95fcc30a53dc5ae655bf2e
aminaouini <aminaouini565@gmail.com>
2020-05-04 08:34:21 +0100

    added doc to register action

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,17 +1,17 @@
 import { REGISTER, OPEN_ACC } from './type';
 import axios from 'axios';
 
-export const registerAction = (creds) => dispatch => {
+export const registerAction = (creds) => dispatch => { // makes post http request to register new account
     axios.post('http://localhost:4404/register', creds)
         .then(res => {
-            let { token } = res.data.results;
-            localStorage.setItem('_____auth_______________token', token);
+            let { token } = res.data.results; // retrive token 
+            localStorage.setItem('_____auth_______________token', token); // set token in local storage
             dispatch({
                 type: REGISTER,
                 openedAcc: true
-            })
+            }) // excute dispatch set openedAcc state to true and add type REGISTER
         })
-        .catch(err => console.log(err));
+        .catch(err => console.log(err)); // console error in case there is one
 }
 
 export const loginAction = creds => dispatch => { // makes post http request to login
