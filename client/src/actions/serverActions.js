8364056bc67a4b6966a69b10e2359e6212e54144
aminaouini <aminaouini565@gmail.com>
2020-05-07 00:59:15 +0100

    fixed create server action to dispatch server name instead of id

--- client/src/actions/serverActions.js
+++ client/src/actions/serverActions.js
@@ -4,11 +4,11 @@ import { axios } from "axios";
 export const createServer = (newServer) => dispatch => {
     axios.post('http://localhost:4404/createServer', newServer)
         .then(res => {
-            let { id } = res.data.results;
+            let { name } = res.data.results;
             console.log('server created')
             dispatch({
                 type: CREATE_SERVER,
-                newServerId: id
+                newServerName: name
             })
         })
         .catch(err => console.log(err));
