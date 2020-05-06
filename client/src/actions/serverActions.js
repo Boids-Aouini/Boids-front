667b1e6f5133b05efe1029ae88f4c72f8e7ad395
aminaouini <aminaouini565@gmail.com>
2020-05-07 00:49:19 +0100

    added dispatch to server action

--- client/src/actions/serverActions.js
+++ client/src/actions/serverActions.js
@@ -4,7 +4,10 @@ import { axios } from "axios";
 export const createServer = (newServer) => dispatch => {
     axios.post('http://localhost:4404/createServer', newServer)
         .then(res => {
-
+            console.log('server created')
+            dispatch({
+                type: CREATE_SERVER
+            })
         })
         .catch(err => console.log(err));
 }
