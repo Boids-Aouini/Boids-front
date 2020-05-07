dd31053f92256bf65d886190d0605fd00ef7d2a0
aminaouini <aminaouini565@gmail.com>
2020-05-07 06:26:31 +0100

    added on newServer method

--- client/src/components/servers/servers.js
+++ client/src/components/servers/servers.js
@@ -3,7 +3,30 @@ import './servers.css';
 import { connect } from 'react-redux';
 import { createServer } from '../../actions/serverActions';
 class Servers extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newServer: ''
+        }
+    }
+    onNewServer() {
+        let newServerName = prompt('Write your new server name')
+        if (newServerName !== null && newServerName === "") { newServerName = prompt('you didn\'t write any thing for your server\'s name') }
+        if (newServerName === "" && newServerName !== null) { newServerName = prompt('if you dont write a server name now your request is going to be canseled') }
+        if (newServerName !== null && newServerName !== "") {
+            let currentDate = new Date();
+            let createdAt = currentDate.getFullYear() + '-' + currentDate.getDay() + '-' + currentDate.getMonth()
+            let newServer = {
+                name: newServerName,
+                createdAt
+            }
+            this.props.createServer(newServer)
 
+        }
+
+
+
+    }
     render() {
         return (
             <div id="servers">
