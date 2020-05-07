1da6830315b4e708f2133515b2f1f51b5b8ba622
aminaouini <aminaouini565@gmail.com>
2020-05-07 04:50:08 +0100

    setup servers front

---
+++ client/src/components/servers/servers.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Servers extends Component {
+    render() {
+        return (
+            <div>
+                <span class="iconify" data-icon="mdi:server-plus" data-inline="false"></span>
+            </div>
+        )
+    }
+}
