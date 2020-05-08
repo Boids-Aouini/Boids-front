5347fed0a1a421d07c32dfae885ba8d8a005aed6
aminaouini <aminaouini565@gmail.com>
2020-05-09 00:17:00 +0100

    fixed server action path

--- client/src/actions/serverActions.js
+++ client/src/actions/serverActions.js
@@ -2,7 +2,7 @@ import { CREATE_SERVER } from './type';
 import axios from "axios";
 
 export const createServer = (newServer) => dispatch => {
-    axios.post('http://localhost:4404/createServer', newServer,
+    axios.post('http://localhost:4404/api/boidsServers/createServer', newServer,
         {
             headers: {
                 'auth_token': localStorage.getItem('_____auth_______________token')
