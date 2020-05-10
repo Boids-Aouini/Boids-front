65c040a53e5b12a3b2dcd9e4f1489198ab5af60e
aminaouini <aminaouini565@gmail.com>
2020-05-10 03:18:41 +0100

    added channels reducer with case get channels server

--- client/src/reducers/channelsReducer.js
+++ client/src/reducers/channelsReducer.js
@@ -1,1 +1,15 @@
-// import { }
+import { GET_CHANNELS_SERVER } from '../actions/type';
+
+let initState = {
+    channels: []
+}
+
+export default function (state = initState, action) {
+    switch (action.type) {
+        case GET_CHANNELS_SERVER:
+            return {
+                channels: action.channels
+            }
+        default: return state;
+    }
+}
