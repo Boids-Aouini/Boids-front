71911a34189ef30034f7d15e981e383967bf96f8
aminaouini <aminaouini565@gmail.com>
2020-05-11 12:35:36 +0100

    added header to new action

--- client/src/actions/membershipsActions.js
+++ client/src/actions/membershipsActions.js
@@ -2,7 +2,12 @@ import axios from 'axios';
 import { ADD_MEMBER } from './type';
 
 export const addMember = newMember => dispatch => {
-    axios.post('/api/memberships/createMembership', newMember)
+    axios.post('/api/memberships/createMembership', newMember,
+        {
+            headers: {
+                'auth_token': localStorage.getItem('_____auth_______________token')
+            }
+        })
         .then(res => {
             dispatch({
                 type: ADD_MEMBER
