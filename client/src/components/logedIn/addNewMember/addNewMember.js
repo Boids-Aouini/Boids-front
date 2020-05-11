828b2570ab073146895ef123396bf01aa8ac884f
aminaouini <aminaouini565@gmail.com>
2020-05-11 12:11:03 +0100

    fixed a bug

--- client/src/components/logedIn/addNewMember/addNewMember.js
+++ client/src/components/logedIn/addNewMember/addNewMember.js
@@ -18,7 +18,7 @@ class AddNewMember extends Component {
         this.setState({ [e.tagert.name]: e.target.value });
     }
     validPage(serverName, serversAsLeader) {
-        for (let server in serversAsLeader) {
+        for (let server of serversAsLeader) {
             if (server.name === serverName) {
                 this.setState({ server_id: server.id });
                 return true;
@@ -30,7 +30,7 @@ class AddNewMember extends Component {
         e.preventDefault()
     }
     render() {
-        return !this.validPage(extractReference(window.location.pathname.split('/')[3]), this.props.server.serversAsLeader) ? (
+        return this.validPage(extractReference(window.location.pathname.split('/')[3]), this.props.servers.serversAsLeader) ? (
             <div>
                 <form>
                     <input onChange={this.onChange.bind(this)} placeholder="Role" type="text" name="role"></input><br></br>
@@ -39,10 +39,10 @@ class AddNewMember extends Component {
                     <button onClick={this.onAdd.bind(this)}>Add Member</button>
                 </form>
             </div>
-        ) : <Redirect to="/" />
+        ) : (<Redirect to="/" />)
     }
 }
 let mapPropsToState = state => ({
-    server: state.serversAsLeader
+    servers: state.servers
 })
 export default connect(mapPropsToState)(AddNewMember);
