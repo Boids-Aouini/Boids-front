3849571b5a9bd1902314473d9a308843f5108682
aminaouini <aminaouini565@gmail.com>
2020-05-11 12:34:26 +0100

    added action add member

--- client/src/actions/membershipsActions.js
+++ client/src/actions/membershipsActions.js
@@ -1,1 +1,12 @@
 import axios from 'axios';
+import { ADD_MEMBER } from './type';
+
+export const addMember = newMember => dispatch => {
+    axios.post('/api/memberships/createMembership', newMember)
+        .then(res => {
+            dispatch({
+                type: ADD_MEMBER
+            })
+        })
+        .catch(err => console.log(err));
+} 
