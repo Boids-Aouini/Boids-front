b27e29bed456e26209d20e055ca9801c5e6481a0
aminaouini <aminaouini565@gmail.com>
2020-05-14 22:30:20 +0100

    added get server id hethod

--- client/src/components/logedIn/workSpace/workSpace.js
+++ client/src/components/logedIn/workSpace/workSpace.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import WorkSpaceNav from './workSpaceNav/workSpaceNav'
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { referenceUrl } from '../../../utils/urlReference';
-import { getChannels, extractReference } from '../../../actions/channelActions';
+import { Link, Redirect } from 'react-router-dom';
+import { referenceUrl, extractReference } from '../../../utils/urlReference';
+import { getChannels } from '../../../actions/channelActions';
 class WorkSpace extends Component {
     constructor(props) {
+        super(props);
         this.state = {
             channel_id: null,
             server_id: null
@@ -16,42 +17,62 @@ class WorkSpace extends Component {
         for (let server of this.props.servers.serversAsLeader) {
             if (server.name === serverName) {
                 setTimeout(() => { this.setState({ server_id: server.id }) }, 0)
-                check++;
+                return true;
             }
         }
+        if (check === 0) {
 
-        for (let server of this.props.servers.serversAsMember) {
-            if (server.name === serverName) {
-                setTimeout(() => { this.setState({ server_id: server.id }) }, 0)
-                check++;
+            for (let server of this.props.servers.serversAsMember) {
+                if (server.name === serverName) {
+                    setTimeout(() => { this.setState({ server_id: server.id }) }, 0)
+                    check++;
+                }
             }
         }
 
         for (let channel of this.props.channels.channels) {
             if (channel.name === channelName) {
-                setTimeout(() => { this.setState({ server_id: server.id }) }, 0)
+                setTimeout(() => { this.setState({ channel_id: channel.id }) }, 0)
                 check++
             }
         }
         return check === 2;
     }
+    getServerId(serverName) {
+        for (let server of this.props.servers.serversAsLeader) {
+            if (server.name === serverName) {
+                setTimeout(() => { this.setState({ server_id: server.id }) }, 0)
+                return true
+            }
+        }
+        for (let server of this.props.servers.serversAsMember) {
+            if (server.name === serverName) {
+                setTimeout(() => { this.setState({ server_id: server.id }) }, 0)
+                return true;
+            }
+        }
+
+    }
+
     componentWillMount() {
-        // this.getChannels(extractReference(window.location.pathname.split('/')[3]))
+        this.getServerId(extractReference(window.location.pathname.split('/')[2]))
+        // this.getChannels(this.state.server_id);
     }
 
     render() {
-        return (
-            <div>
-                <WorkSpaceNav />
+        return this.getIds(extractReference(window.location.pathname.split('/')[2]), extractReference(window.location.pathname.split('/')[3]))
+            ? (
                 <div>
-                    {this.props.channels.channels.map((channel, i) => (
-                        <div>
-                            <Link to={'/boidsServer/' + window.location.pathname.split('/')[2] + '/' + referenceUrl(channel.name)}>{channel.name}</Link>
-                        </div>
-                    ))}
+                    <WorkSpaceNav />
+                    <div>
+                        {this.props.channels.channels.map((channel, i) => (
+                            <div>
+                                <Link to={'/boidsServer/' + window.location.pathname.split('/')[2] + '/' + referenceUrl(channel.name)}>{channel.name}</Link>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-            </div>
-        )
+            ) : <Redirect to="/" />
     }
 }
 let mapPropsToState = state => ({
