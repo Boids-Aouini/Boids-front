90d00b0a69d4f78c0952f2459c233d72f646842d
aminaouini <aminaouini565@gmail.com>
2020-05-14 10:47:57 +0100

    display links to channels

--- client/src/components/logedIn/workSpace/workSpace.js
+++ client/src/components/logedIn/workSpace/workSpace.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
 import WorkSpaceNav from './workSpaceNav/workSpaceNav'
 import { connect } from 'react-redux';
-
+import { Link } from 'react-router-dom';
+import { extractReference } from '../../../utils/urlReference'
 class WorkSpace extends Component {
     render() {
         return (
             <div>
                 <WorkSpaceNav />
                 <div>
-
+                    {this.props.channels.channels.map((channel, i) => (
+                        <div>
+                            <Link to={'/boidsServer/' + window.location.pathname.split('/')[2] + '/' + extractReference(channel.name)}>{channel.name}</Link>
+                        </div>
+                    ))}
                 </div>
             </div>
         )
