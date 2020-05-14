432b611f90be3da4fa3e17cbde50364d81688139
aminaouini <aminaouini565@gmail.com>
2020-05-14 10:34:34 +0100

    setup channels state to workSpace comp

--- client/src/components/logedIn/workSpace/workSpace.js
+++ client/src/components/logedIn/workSpace/workSpace.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 import WorkSpaceNav from './workSpaceNav/workSpaceNav'
+import { connect } from 'react-redux';
 
-export default class WorkSpace extends Component {
+class WorkSpace extends Component {
     render() {
         return (
             <div>
                 <WorkSpaceNav />
+                <div>
+
+                </div>
             </div>
         )
     }
 }
+let mapPropsToState = state => ({
+    channels: state.channels
+})
+
+export default connect(mapPropsToState)(WorkSpace);
