e2bbc6c55b4d9038590b7c2e8c884dba03463737
aminaouini <aminaouini565@gmail.com>
2020-05-16 05:45:50 +0100

    added doc to action

--- client/src/actions/serverActions.js
+++ client/src/actions/serverActions.js
@@ -3,20 +3,20 @@ import axios from "axios";
 
 export const createServer = (newServer) => dispatch => {
     axios.post('http://localhost:4404/api/boidsServers/createServer', newServer,
+        // make post request to make new server
         {
             headers: {
-                'auth_token': localStorage.getItem('_____auth_______________token')
+                'auth_token': localStorage.getItem('_____auth_______________token') // add token as a header
             }
         })
         .then(res => {
             let { server } = res.data.results;
-            console.log('server created')
-            dispatch({
+            dispatch({ // dispatch new server to serversAsLeader redux's state
                 type: CREATE_SERVER,
                 server
             })
         })
-        .catch(err => console.log(err));
+        .catch(err => console.log(err)); // console error in case there is one
 }
 
 export const retreiveServerAsLeader = () => dispatch => {
