f118706691b79152a94c90aac61301b743532e64
aminaouini <aminaouini565@gmail.com>
2020-05-16 10:10:35 +0100

    setup channels nav comp

---
+++ client/src/components/logedIn/workSpace/channelsNav/channelsNav.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+
+class ChannelsNav extends Component {
+    render() {
+        return (
+            <div>
+                {this.props.channels.channels.map((channel, i) => (
+                    <div key={i}>
+                        <Link to={'/boidsServer/' + window.location.pathname.split('/')[2] + '/' + referenceUrl(channel.name)}>{channel.name}</Link>
+                    </div>
+                ))}
+            </div>
+        )
+    }
+}
+
+let mapPropsToState = state => ({
+    channels: state.channels
+})
+
+export default connect(mapPropsToState)(ChannelsNav);
