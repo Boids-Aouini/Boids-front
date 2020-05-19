a2410c5087a76d6857b058929abc5f2976c7e515
aminaouini <aminaouini565@gmail.com>
2020-05-19 16:57:30 +0100

    added test NotlogedNav comp

--- client/src/__test__/components/navbar/notLogedNav.test.js
+++ client/src/__test__/components/navbar/notLogedNav.test.js
@@ -2,7 +2,18 @@ import React from 'react';
 import toJson from 'enzyme-to-json';
 import { shallow } from 'enzyme';
 import configureStore from 'redux-mock-store';
+import NotLogedNav from '../../../components/navbar/notLogedNav';
 
 let mockStore = configureStore();
 let initState = {};
 let store = mockStore(initState);
+
+describe('<NotLogedNav />', () => {
+    describe('render()', () => {
+        test('should render without crashing', () => {
+            shallow(<NotLogedNav store={store} />)
+        })
+
+    })
+
+})
