b5c3837660e80d543aefa4fc77d34eff83e0a138
aminaouini <aminaouini565@gmail.com>
2020-05-20 00:58:40 +0100

    fixed render test problem

--- client/src/__test__/components/navbar/notLogedNav.test.js
+++ client/src/__test__/components/navbar/notLogedNav.test.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import toJson from 'enzyme-to-json';
 import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import NotLogedNav from '../../../components/navbar/notLogedNav';
 
 describe('<NotLogedNav />', () => {
     describe('render()', () => {
-        test('should render without crashing', () => {
+        test('render without crashing', () => {
             shallow(<NotLogedNav />)
         })
-        test('should render component', () => {
-            let wrapper = shallow(<NotLogedNav />);
-            let component = wrapper.dive();
-            expect(toJson(component)).toMatchSnapshot();
+        test('render component', () => {
+            let wrapper = shallow(<NotLogedNav />)
+            expect(toJson(wrapper)).toMatchSnapshot()
         })
-
     })
 
 })
