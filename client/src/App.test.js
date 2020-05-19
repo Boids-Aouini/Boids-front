74c213f446e39db065e3690164f5467c1f1fe5ea
aminaouini <aminaouini565@gmail.com>
2020-05-19 06:27:54 +0100

    setup test app with enzyme and added mock store to app.test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+let mockStore = configureStore();
+let initialState = {}
+let store = mockStore(initialState)
+test('render without crashing', () => {
+  shallow(<App store={store} />);
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
