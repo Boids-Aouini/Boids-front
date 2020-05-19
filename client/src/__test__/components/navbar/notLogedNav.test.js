88884c51da2fd6fda70f460ebc68a9d667b9e511
aminaouini <aminaouini565@gmail.com>
2020-05-19 20:16:01 +0100

    removed not needed code

--- client/src/__test__/components/navbar/notLogedNav.test.js
+++ client/src/__test__/components/navbar/notLogedNav.test.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import toJson from 'enzyme-to-json';
 import { shallow } from 'enzyme';
-import configureStore from 'redux-mock-store';
 import NotLogedNav from '../../../components/navbar/notLogedNav';
-
-let mockStore = configureStore();
-let initState = {};
-let store = mockStore(initState);
 
 describe('<NotLogedNav />', () => {
     describe('render()', () => {
         test('should render without crashing', () => {
-            shallow(<NotLogedNav store={store} />)
+            shallow(<NotLogedNav />)
         })
         test('should render component', () => {
-            let wrapper = shallow(<NotLogedNav store={store} />)
-            let comp = wrapper.dive();
-            expect(toJson(comp)).toMatchSnapshot();
+            let wrapper = shallow(<NotLogedNav />);
+            let component = wrapper.dive();
+            expect(toJson(component)).toMatchSnapshot();
         })
 
 
