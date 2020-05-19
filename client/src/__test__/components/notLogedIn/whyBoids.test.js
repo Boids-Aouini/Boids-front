24b62ce65ab78d374032a932184482744f853ddc
aminaouini <aminaouini565@gmail.com>
2020-05-19 15:38:07 +0100

    added test render to whyboids comp

--- client/src/__test__/components/notLogedIn/whyBoids.test.js
+++ client/src/__test__/components/notLogedIn/whyBoids.test.js
@@ -2,7 +2,17 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import configureStore from 'redux-mock-store';
+import WhyBoids from '../../../components/notLogedIn/whyBoids';
 
 let mockStore = configureStore();
 let initState = {};
 let store = mockStore(initState);
+
+describe('<WhyBoids />', () => {
+    describe('render()', () => {
+        test('render without crashing', () => {
+            shallow(<WhyBoids store={store} />)
+        })
+    })
+
+})
