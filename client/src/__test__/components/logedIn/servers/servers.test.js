b3bb7c5fdf1adfe1f9fd5b25464491e0f5a86d6b
aminaouini <aminaouini565@gmail.com>
2020-05-19 06:52:56 +0100

    added render without crashing test to servers comp

--- client/src/__test__/components/logedIn/servers/servers.test.js
+++ client/src/__test__/components/logedIn/servers/servers.test.js
@@ -1,6 +1,9 @@
+import React from 'react';
 import toJson from 'enzyme-to-json';
 import configureStore from 'redux-mock-store';
-import { shallow } from 'enzyme'
+import { shallow } from 'enzyme';
+import Servers from '../../../../components/logedIn/servers/servers';
+
 let mockStore = configureStore();
 let initState = {};
 let store = mockStore(initState);
@@ -8,7 +11,7 @@ let store = mockStore(initState);
 describe('<Servers />', () => {
     describe('render()', () => {
         test('render without crashing', () => {
-
+            shallow(<Servers store={store} />);
         })
     })
 })
