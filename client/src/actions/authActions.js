9acc226d38a9a5c6257797f2f528931065844a2d
aminaouini <aminaouini565@gmail.com>
2020-05-20 19:16:02 +0100

    working to resolve dispatch store prob

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -15,11 +15,11 @@ export const registerAction = (creds) => dispatch => { // makes post http reques
 }
 
 export const loginAction = creds => dispatch => { // makes post http request to login
-    return axios.post('http://localhost:4404/api/auth/login', creds)
+    axios.post('http://localhost:4404/api/auth/login', creds)
         .then(res => {
             let { token } = res.data.results; // retreive token
             localStorage.setItem('_____auth_______________token', token); // set token in local storage
-            return dispatch({
+            dispatch({
                 type: OPEN_ACC,
                 openedAcc: localStorage.getItem('_____auth_______________token')
             }) // excute dispatch set openedAcc state to token that we set in local storage and add type OPEN_ACC to acion
