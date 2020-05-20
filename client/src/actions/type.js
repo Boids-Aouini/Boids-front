6a1a95f6bb6d11c2f97b02057a59cd4b6221c70f
aminaouini <aminaouini565@gmail.com>
2020-05-20 14:40:34 +0100

    organized types

--- client/src/actions/type.js
+++ client/src/actions/type.js
@@ -1,8 +1,15 @@
+// auth types
 export const REGISTER = "REGISTER";
 export const OPEN_ACC = "OPEN_ACC";
 export const LOG_OUT = "LOG_OUT";
+
+// servers types
 export const CREATE_SERVER = "CREATE_SERVER";
 export const RETREIVE_SERVER_AS_LEADER = 'RETREIVE_SERVER_AS_LEADER';
+
+// memberships types
 export const ADD_MEMBER = 'ADD_MEMBER';
+
+// channels types
 export const GET_CHANNELS_SERVER = 'GET_CHANNELS_SERVER';
 export const MAKE_CHANNEL = 'MAKE_CHANNEL';
