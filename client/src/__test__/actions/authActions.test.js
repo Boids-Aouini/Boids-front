9408ec2d3a5eac37887a70a1d88676ed14d547dc
aminaouini <aminaouini565@gmail.com>
2020-05-20 14:22:08 +0100

    added acc data to login

--- client/src/__test__/actions/authActions.test.js
+++ client/src/__test__/actions/authActions.test.js
@@ -1,5 +1,6 @@
 import * as authActions from '../../actions/authActions';
 import configureStore from 'redux-mock-store';
+import { OPEN_ACC, REGISTER } from '../../actions/type';
 
 let mockStore = configureStore();
 let store = mockStore();
@@ -7,5 +8,11 @@ let store = mockStore();
 describe('authActions', () => {
     beforeEach(() => {
         store.clearActions();
+    })
+    test('OPEN_ACC Action', () => {
+        let account = {
+            email: 'aminaouini565@gmail.com',
+            password: '123456789'
+        }
     })
 })
