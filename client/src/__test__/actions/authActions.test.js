6c8714db86448f5077e1ee2b9c971cdba255d9a2
aminaouini <aminaouini565@gmail.com>
2020-05-21 03:43:48 +0100

    fixing a prob

--- client/src/__test__/actions/authActions.test.js
+++ client/src/__test__/actions/authActions.test.js
@@ -4,20 +4,21 @@ import { OPEN_ACC, REGISTER } from '../../actions/type';
 import thunk from 'redux-thunk';
 let middelwares = [thunk];
 let mockStore = configureStore(middelwares);
+let store = mockStore({});
 
 describe('authActions', () => {
     beforeEach(() => {
         store.clearActions();
     })
     test('OPEN_ACC Action', () => {
-        let store = mockStore({});
         let account = {
             email: 'aminaouini565@gmail.com',
             password: '123456789'
         }
         return store.dispatch(authActions.loginAction(account))
             .then(() => {
-                const actions = store.getActions();
+                const actions = store.getActions()
+                console.log(actions)
                 expect(actions[0]['type']).toEqual(OPEN_ACC)
             })
     })
