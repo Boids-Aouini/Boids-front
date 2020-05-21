a78eb10f1fa43a3f957810866dadd561cada2c3f
aminaouini <aminaouini565@gmail.com>
2020-05-21 07:17:59 +0100

    refactor get channels to return dispatch

--- client/src/actions/channelActions.js
+++ client/src/actions/channelActions.js
@@ -2,7 +2,7 @@ import { GET_CHANNELS_SERVER, MAKE_CHANNEL } from './type';
 import axios from 'axios';
 
 export const getChannels = (server_id) => dispatch => {
-    axios.get('http://localhost:4404/api/channels/getChannels/' + server_id, {
+    return axios.get('http://localhost:4404/api/channels/getChannels/' + server_id, {
         // make get request to reteive channels and added server_id as a param
         headers: {
             'auth_token': localStorage.getItem('_____auth_______________token') // setup token as a header
@@ -10,7 +10,7 @@ export const getChannels = (server_id) => dispatch => {
     })
         .then(res => {
             let { channels } = res.data.results;
-            dispatch({ // dispatch new channels to redux's state
+            return dispatch({ // dispatch new channels to redux's state
                 channels,
                 type: GET_CHANNELS_SERVER
             })
