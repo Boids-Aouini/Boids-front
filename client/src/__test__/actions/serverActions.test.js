de3004f6123aab2dbf1ba5b66fdca18104ea4014
aminaouini <aminaouini565@gmail.com>
2020-05-21 06:09:39 +0100

    added localstorage mock to server actions

--- client/src/__test__/actions/serverActions.test.js
+++ client/src/__test__/actions/serverActions.test.js
@@ -2,7 +2,9 @@ import * as serverActions from '../../actions/serverActions';
 import configureStore from 'redux-mock-store';
 import { RETREIVE_SERVER_AS_LEADER, CREATE_SERVER } from '../../actions/type';
 import thunk from 'redux-thunk';
-
+import { LocalStorageMock, token } from '../../components/utils/test/storageMock';
 let middlewares = [thunk];
 let mockStore = configureStore(middlewares);
 let store = mockStore({})
+window.localStorage = new LocalStorageMock;
+localStorage.setItem('_____auth_______________token', token);
