c5571405081f1ab5579aeffb6ad8ef6b8a3d7cf4
aminaouini <aminaouini565@gmail.com>
2020-05-21 15:20:14 +0100

    added message to combine reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,12 @@ import authReducer from './authReducer';
 import serverReducer from './serversReducer';
 import memberShipReducer from './memebershipReducer';
 import channelReducer from './channelsReducer';
+import messageReducer from './messagesReducer';
 
 export default combineReducers({
     auth: authReducer,
     servers: serverReducer,
     memberShip: memberShipReducer,
-    channels: channelReducer
+    channels: channelReducer,
+    messages: messageReducer
 })
