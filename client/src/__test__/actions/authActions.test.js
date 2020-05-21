5d268da9ac4482c0368af4092c7f6b6e97e2fa92
aminaouini <aminaouini565@gmail.com>
2020-05-21 03:36:22 +0100

    working on testing actions

--- client/src/__test__/actions/authActions.test.js
+++ client/src/__test__/actions/authActions.test.js
@@ -1,20 +1,24 @@
-import { loginAction } from '../../actions/authActions';
+import * as authActions from '../../actions/authActions';
 import configureStore from 'redux-mock-store';
 import { OPEN_ACC, REGISTER } from '../../actions/type';
-
-let mockStore = configureStore();
-let store = mockStore();
+import thunk from 'redux-thunk';
+let middelwares = [thunk];
+let mockStore = configureStore(middelwares);
 
 describe('authActions', () => {
     beforeEach(() => {
         store.clearActions();
     })
     test('OPEN_ACC Action', () => {
+        let store = mockStore({});
         let account = {
             email: 'aminaouini565@gmail.com',
             password: '123456789'
         }
-        store.dispatch(loginAction(account))
-        expect(store.getActions()[0]['type']).toEqual(OPEN_ACC)
+        return store.dispatch(authActions.loginAction(account))
+            .then(() => {
+                const actions = store.getActions();
+                expect(actions[0]['type']).toEqual(OPEN_ACC)
+            })
     })
 })
