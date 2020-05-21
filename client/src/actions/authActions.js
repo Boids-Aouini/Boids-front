2d7a8aadba35cf78656872f8c956c4918d129521
aminaouini <aminaouini565@gmail.com>
2020-05-21 04:17:51 +0100

    fixed register action for testing

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -2,11 +2,11 @@ import { REGISTER, OPEN_ACC, LOG_OUT } from './type';
 import axios from 'axios';
 
 export const registerAction = (creds) => dispatch => { // makes post http request to register new account
-    axios.post('http://localhost:4404/api/auth/register', creds)
+    return axios.post('http://localhost:4404/api/auth/register', creds)
         .then(res => {
             let { token } = res.data.results; // retrive token 
             localStorage.setItem('_____auth_______________token', token); // set token in local storage
-            dispatch({
+            return dispatch({
                 type: REGISTER,
                 openedAcc: localStorage.getItem('_____auth_______________token')
             }) // excute dispatch set openedAcc state to token that we set in local storage and add type REGISTER to action
