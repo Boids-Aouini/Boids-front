f38fabaf8f89ed78cfaac8031408cb6e2d7c4b1a
aminaouini <aminaouini565@gmail.com>
2020-05-23 12:39:37 +0100

    added onChangeChannel method

--- client/src/components/logedIn/workSpace/channelsNav/channelsNav.js
+++ client/src/components/logedIn/workSpace/channelsNav/channelsNav.js
@@ -4,13 +4,18 @@ import './channelsNav.css'
 import { Link } from 'react-router-dom';
 import { referenceUrl } from '../../../utils/urlReference';
 import { shortChannelName } from '../../../utils/shortName';
+import { currentChannel, getPosts } from '../../../../actions/channelActions'
 class ChannelsNav extends Component {
+    onChangeChannel(channel_id) {
+        this.props.currentChannel(channel_id)
+        this.props.getPosts(this.props.servers.currentServer, channel_id)
+    }
     render() {
         return (
             <div id="channelsNav">
                 {this.props.channels.channels.map((channel, i) => (
                     <div key={i}>
-                        <Link class="channelLink" to={'/boidsServer/' + window.location.pathname.split('/')[2] + '/' + referenceUrl(channel.name)}>
+                        <Link onClick={() => this.onChangeChannel(channel.id)} class="channelLink" to={'/boidsServer/' + window.location.pathname.split('/')[2] + '/' + referenceUrl(channel.name)}>
                             <span class="iconify" data-icon="uil:channel" data-inline="false"></span>
                             {shortChannelName(channel.name)}
                         </Link>
@@ -23,7 +28,8 @@ class ChannelsNav extends Component {
 }
 
 let mapPropsToState = state => ({
-    channels: state.channels
+    channels: state.channels,
+    servers: state.servers
 })
 
-export default connect(mapPropsToState)(ChannelsNav);
+export default connect(mapPropsToState, { currentChannel, getPosts })(ChannelsNav);
