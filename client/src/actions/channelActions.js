bb2a32310fe2ed54fc1b5f0ac260f8ef2f7770bd
aminaouini <aminaouini565@gmail.com>
2020-05-24 19:05:25 +0100

    fixed missing type

--- client/src/actions/channelActions.js
+++ client/src/actions/channelActions.js
@@ -1,4 +1,4 @@
-import { GET_CHANNELS_SERVER, MAKE_CHANNEL, SEND_POST, GET_POSTS, CHANGE_CURRENT_CHANNEL } from './type';
+import { GET_CHANNELS_SERVER, MAKE_CHANNEL, SEND_POST, GET_POSTS, CHANGE_CURRENT_CHANNEL, ADD_POST } from './type';
 import axios from 'axios';
 
 export const getChannels = (server_id) => dispatch => {
