86c05fc85f23139eabad8f45f2fa9bbbe9bc84e5
aminaouini <aminaouini565@gmail.com>
2020-05-27 12:26:44 +0100

    added media query for front

--- client/src/components/notLogedIn/landingPage.js
+++ client/src/components/notLogedIn/landingPage.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-
+import './landingPage.css'
 class LandingPage extends Component {
     render() {
         return (
-            <div>
-                <h1>Boids gathers separated teams in one platform</h1>
+            <div id="landingContent">
+                <h1 id="mainContent">Boids gathers separated teams in one platform</h1>
                 <p>so you can stay productive you and your team</p>
                 {!this.props.auth.openedAcc ? (<Link to="/register">Register</Link>) : (<span></span>)}
             </div>
