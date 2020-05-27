928631abf5306fcc20964f0c1406cb97bfd517a4
aminaouini <aminaouini565@gmail.com>
2020-05-27 22:11:03 +0100

    added added member reducer case

--- client/src/reducers/memebershipReducer.js
+++ client/src/reducers/memebershipReducer.js
@@ -1,12 +1,16 @@
-import { ADD_MEMBER } from '../actions/type';
+import { ADD_MEMBER, ADDED_MEMBER } from '../actions/type';
 
 let initState = {
-
+    addedMember: false
 }
 
 export default function (state = initState, action) {
     switch (action.type) {
         case ADD_MEMBER: return state; // return state
+        case ADDED_MEMBER:
+            return {
+                addedMember: !state.addedMember
+            }
         default: return state;
     }
 }
