4ac4a4b0514cace9c47422b75ea805528ca37b2b
M. Amine Aouini <aminaouini565@gmail.com>
2020-05-30 05:20:39 +0200

    fixed style while rendering

--- client/src/components/logedIn/workSpace/channelsNav/channelsNav.js
+++ client/src/components/logedIn/workSpace/channelsNav/channelsNav.js
@@ -7,16 +7,41 @@ import { shortChannelName } from '../../../utils/shortName';
 import { extractReference } from '../../../utils/urlReference'
 import { currentChannel, getPosts } from '../../../../actions/channelActions'
 class ChannelsNav extends Component {
-
+    constructor(props){
+        super(props)
+        this.state = {
+            max780: null
+        }
+    }
     onChangeChannel(channel_id) {
         this.props.currentChannel(channel_id)
         this.props.getPosts(this.props.servers.currentServer, channel_id)
     }
 
+    checkServerinLeader(){
+        let place = window.location.pathname.split('/')[1];
+        let serverName = place === 'boidsServer' ? window.location.pathname.split('/')[2] : place === 'options' ?
+            window.location.pathname.split('/')[3] : null;
+        
+        for(let server of this.props.servers.serversAsLeader){
+            if(server.name === serverName) return true;
+        }
+    }
+
+    checkMedia(){
+        let checkMedia = window.matchMedia('(max-width: 780px)')
+        setTimeout(()=>this.setState({max780: checkMedia.matches}), 0)
+        return true
+    }
+
     render() {
 
-        return (
-            <div id="channelsNav" >
+        return this.checkMedia() ? (
+            <div id="channelsNav" style={
+                this.state.max780 ? {
+                    marginTop: (!this.checkServerinLeader()? '16%': '23%')
+                } : {}
+            }>
                 {this.props.channels.channels.map((channel, i) => (
                     <div key={i}>
                         <Link onClick={() => this.onChangeChannel(channel.id)} class="channelLink" to={'/boidsServer/' + window.location.pathname.split('/')[2] + '/' + referenceUrl(channel.name)}>
@@ -27,7 +52,7 @@ class ChannelsNav extends Component {
                 ))}
             </div>
 
-        )
+        ) : <></>
     }
 }
 
